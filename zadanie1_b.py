6dbdf41a496ee9211a71a8f1d00ba6e82903f2d1
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-02-14 00:04:22 +0300

    new file zadanie1_b.py

---
+++ zadanie1_b.py
@@ -0,0 +1,11 @@
+try:
+    a = list(map(int, input('Введите катеты: ').split()))
+    n = int(len(a)/2)
+    for i in range(n, 2):
+        c = (a[i]**2+a[i+1]**2)**0.5
+        S = a[i]*a[i+1]/2
+        print(f'Треугольник {i+1} с катетами {a[i]} и {a[i+1]} имеет площадь {S:.1f} и гипотенузу {c:.1f}')
+except ValueError:
+    print('NonNumericError: Вы ввели не число')
+except IndexError:
+    print('InconsistentDataError: массивы с катетами имеют разную длину')
