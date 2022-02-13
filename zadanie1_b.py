3a63b70d014000196345799e7ad906085d4b075d
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-02-14 00:18:51 +0300

    changed file zadanie1_b.py

--- zadanie1_b.py
+++ zadanie1_b.py
@@ -1,7 +1,6 @@
 try:
     a = list(map(int, input('Введите катеты: ').split()))
-    n = int(len(a)/2)
-    for i in range(n, 2):
+    for i in range(0, len(a), 2):
         c = (a[i]**2+a[i+1]**2)**0.5
         S = a[i]*a[i+1]/2
         print(f'Треугольник {i+1} с катетами {a[i]} и {a[i+1]} имеет площадь {S:.1f} и гипотенузу {c:.1f}')
